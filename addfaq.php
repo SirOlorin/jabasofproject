79066328899418f3aab0dd345d8ca1c720e10325
alaonn <alain.nguyen@isep.fr>
2019-01-15 14:50:37 +0100

    Création de la fonctionnalité d'ajout d'une nouvelle question dans la FAQ avec un contrôle de formulaire et une insertion dans la base de données.

--- addfaq.php
+++ addfaq.php
@@ -1,7 +1,33 @@
+<link rel="stylesheet" type="text/css" href="accueil.css">
+<center><h2 style="font-family:sans-serif; color: white; font-size: 50px;">Ajouter un article à l'accueil</h2></center>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: pikal
- * Date: 15/01/2019
- * Time: 01:36
- */
+$bdd = new PDO("mysql:host=127.0.0.1;dbname=jabasof;charset=utf8", "root", "");
+if(isset($_POST['submit'])) {
+    if( !empty($_POST['question']) AND !empty($_POST['content'])) {
+        $question = htmlspecialchars($_POST['question']);
+        $content = htmlspecialchars($_POST['content']);
+        $set = $bdd->prepare("INSERT INTO `pagefaq`(`question`, `content`) VALUES (?,?)");
+        $set -> execute(array($question, $content));
+        $message="La question a bien été ajoutée";
+        //header('Location: index.php?page=boaccueil ');
+        header( "refresh:2;url=index.php?page=bofaq" );
+    }else {
+        $message = "Veuillez remplir tous les champs du formulaire.";
+    }
+}
+
+if (isset ($message)) {
+    echo $message;
+}
+?>
+
+<form action="" method="post" id="adminaccueil">
+    <br>
+    <textarea rows="1" cols="60" placeholder="Titre" name="question" form="adminaccueil"></textarea>
+    <br>
+    <textarea rows="10" cols="60" placeholder="Contenu" name="content" form="adminaccueil"></textarea>
+    <br>
+    <input type="submit" name="submit" value="Ajouter la question">
+    <br>
+</form>
