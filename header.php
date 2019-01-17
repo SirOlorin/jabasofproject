151467b60f069863c6d5f110c6693c075558aa1e
alaonn <alain.nguyen@isep.fr>
2019-01-17 22:28:33 +0100

    gestion d'affichage du contenu en fonction de la session et ajout de session start dans ce fichier

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html lang="en">
 
 <head>
@@ -13,13 +16,25 @@
             <li><a href="?page=accueil">Accueil</a>
                 <div class="subwrap">
                     <ul class="sub">
-                        <li><a href="#block page">Qui sommes-nous ?</a></li>
-                        <li><a href="?page=connexion">Connexion</a></li>
-                        <li><a href="?page=inscription">Inscription</a></li>
-
+                        <li><a href="#presentation">Qui sommes-nous ?</a></li>
                     </ul>
                 </div>
             </li>
+
+            <li><a href="#">Aide</a>
+                <div class="subwrap">
+                    <ul class="sub">
+                        <li><a href="?page=forum">Forum</a></li>
+                        <li><a href="?page=faq">FAQ</a></li>
+                        <li><a href="?page=support">Support</a></li>
+                </div>
+            </li>
+
+            <?php
+
+            if(isset($_SESSION['signed_in'])) {
+                $currentid = $_SESSION['signed_in'];
+                echo '
             <li><a href="?page=tableaudebord">Tableau de bord</a>
                 <div class="subwrap">
                     <ul class="sub">
@@ -27,14 +42,6 @@
                         <li><a href="?page=reveil">Réveil</a></li>
                         <li><a href="?page=securite">Sécurité</a></li>
                     </ul>
-                </div>
-            </li>
-            <li><a href="#">Aide</a>
-                <div class="subwrap">
-                    <ul class="sub">
-                        <li><a href="?page=forum">Forum</a></li>
-                        <li><a href="?page=faq">FAQ</a></li>
-                        <li><a href="?page=support">Support</a></li>
                 </div>
             </li>
             <li><a>Paramètres</a>
@@ -45,10 +52,39 @@
                     </ul>
                 </div>
             </li>
-            <li><a href="?page=admin">Admin</a>
+                <li><a href="?page=member">Espace Membre</a>
                 <div class="subwrap">
                 </div>
             </li>
+            <li><a href="?page=logout">Déconnexion</a>
+                <div class="subwrap">
+                </div>
+            </li>
+                ';
+            }else{
+                echo '
+                <li><a href="?page=inscription">S\'inscrire</a>
+                <div class="subwrap">
+                </div>
+            </li>
+            <li><a href="?page=connexion">Se connecter</a>
+                <div class="subwrap">
+                </div>
+            </li>
+                ';
+            }
+            if(isset($_SESSION['user_id'])) {
+                if ($_SESSION['user_id']==1){
+                    $currentid = $_SESSION['user_id'];
+                    echo '
+                <li><a href="?page=admin">Admin</a>
+                    <div class="subwrap">
+                    </div>
+                </li>
+                        ';
+                }
+            }
+            ?>
         </ul>
     </nav>
 </header>
