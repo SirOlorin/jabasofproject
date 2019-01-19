14de16982b3f92c523103273e06bf9c56071d2d6
alaonn <alain.nguyen@isep.fr>
2019-01-19 21:11:47 +0100

    Ajour de la gestion de profil, possibilité d'afficher une page de profil avec données personnelles et activité du compte, possibilité de modifier son profil

--- member.php
+++ member.php
@@ -1,7 +1,91 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pikal
- * Date: 17/01/2019
- * Time: 21:20
- */
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=jabasof', 'root', '',array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+?>
+
+<link rel="stylesheet" type="text/css" href="member.css">
+
+<center><h2 style="font-family:sans-serif; color: white; font-size: 50px;">Espace Membre</h2></center>
+
+
+    <div class="container">
+        <div class="subcontainers">
+            <?php
+            $getinfos = $bdd->query("SELECT * FROM `users` 
+                                                    WHERE users.user_id = ".$_SESSION['user_id']);
+            while($result=$getinfos->fetch()){
+                echo
+                    '        
+                        <h2>Informations du compte</h2>
+
+                            <h3>Nom d\'utilisateur</h3>
+                            <p>'.$result['user_name'].'</p>
+                            <form action="?page=memberedit&value=user_name" method="post">
+                                <input type="submit" value="Modifier le nom d\'utilisateur" />
+                            </form>
+                        
+                            <h3>Prénom</h3> 
+                            <p>'.$result['user_firstname'].'</p>
+                            <form action="?page=memberedit&value=user_firstname" method="post">
+                                <input type="submit" value="Modifier le prénom" />
+                            </form>
+                        
+                            <h3>Nom</h3>
+                            <p>'.$result['user_lastname'].'</p>
+                            <form action="?page=memberedit&value=user_lastname" method="post">
+                                <input type="submit" value="Modifier le nom de famille" />
+                            </form>
+                        
+                            <h3>Mot de passe</h3>
+                            <p>[Caché]</p>
+                            <form action="?page=memberedit&value=user_pass" method="post">
+                                <input type="submit" value="Modifier le mot de passe" />
+                            </form>
+                       
+                            <h3>Adresse Mail</h3>
+                            <p>'.$result['user_email'].'</p>
+                            <form action="?page=memberedit&value=user_email" method="post">
+                                <input type="submit" value="Modifier l\'adresse mail" />
+                            </form>
+                       
+                        
+                '
+                ;
+            }
+            ?>
+
+
+        </div>
+
+
+        <div class="subcontainers">
+            <h2>Participation au Forum</h2>
+            <h3>Sujet créé</h3>
+
+            <p></p>
+            <h3>Messages postés</h3>
+            <p></p>
+        </div>
+
+        <div class="subcontainers">
+            <h2>Mes Maisons</h2>
+            <?php
+            $gethouses = $bdd->query("SELECT * FROM `users` 
+                                                    INNER JOIN `houselinks` ON users.user_id = houselinks.user_id
+                                                    INNER JOIN `houses` ON houses.house_id = houselinks.house_id
+                                                    WHERE users.user_id =".$_SESSION['user_id']);
+            while($result=$gethouses->fetch()){
+                echo
+                '        
+                        <p>'.$result['house_name'].'</p>
+                '
+                ;
+            }
+            ?>
+
+        </div>
+
+        <div class="subcontainers">
+            <h2>Fonctionnalités supplémentaires</h2>
+        </div>
+
+    </div>
---
+++ memberedit.php
@@ -0,0 +1,125 @@
+<link rel="stylesheet" type="text/css" href="memberedit.css">
+<center><h2 style="font-family:sans-serif; color: white; font-size: 50px;">Modifier le profil</h2></center>
+
+<?php
+$bdd = new PDO("mysql:host=127.0.0.1;dbname=jabasof;charset=utf8", "root", "");
+
+
+
+if(isset($_GET['value'], $_SESSION['user_id'])) {
+    $id=htmlspecialchars($_SESSION['user_id']);
+    $value = htmlspecialchars($_GET['value']);
+}else{
+    $message= 'Aucune information sélectionnée.';
+}
+
+
+$getcontent = $bdd->prepare("SELECT * FROM `users` WHERE user_id = ?");
+$getcontent -> execute(array($id));
+
+if ($value=='user_pass'){
+    $displayvalue = 'Mot de passe';
+}else if ($value=='user_name'){
+    $displayvalue = 'Nom d\'utilisateur';
+}else if ($value=='user_lastname'){
+    $displayvalue = 'Nom de famille';
+}else if ($value=='user_firstname'){
+    $displayvalue = 'Prénom';
+}else{
+    $displayvalue = 'Adresse E-Mail';
+}
+
+while($result=$getcontent->fetch()) {
+    if ($value=='user_pass'){
+        echo '
+        <h3>Modification du mot de passe</h3>
+        <form action="" method="post">
+            <label>Ancien mot de passe</label>
+            <input type="password" name="oldpassword"><br>
+            <label>Nouveau mot de passe</label>
+            <input type="password" name="password"><br>
+            <label>Confirmation du nouveau mot de passe</label>
+            <input type="password" name="password2"><br>
+            <input type="submit" name="mdp">
+        </form>
+        ';
+    }else{
+        echo '
+        <h3>Modification : '.$displayvalue.'</h3>
+        <form action="" method="post">
+            <label>'.$displayvalue.' : '.$result[$value].'</label><br>
+            <label>Nouvelle valeur : </label>
+            <input type="text" name="newvalue"><br>
+            <input type="submit" name="submit">
+        </form>
+';
+    }
+
+
+}
+
+if(isset($_POST['mdp'], $_SESSION['user_id'])) {
+    if( !empty($_POST['password']) AND ($_POST['password2'])) {
+        $id = $_SESSION['user_id'];
+        $password = sha1($_POST['password']);
+        $password2 = sha1($_POST['password2']);
+        $oldpassword = sha1($_POST['oldpassword']);
+        $getpassword = $bdd->prepare("SELECT * FROM `users` WHERE user_id = ?");
+        $getpassword -> execute(array($id));
+        while($result=$getpassword->fetch()) {
+            $currentpassword = $result['user_pass'];
+        }
+        if ($currentpassword==$oldpassword){
+            if ($password==$password2){
+                $set = $bdd->prepare("UPDATE `users` SET `user_pass`=? WHERE user_id = ?");
+                $set -> execute(array($password, $id));
+                $message="Le mot de passe a bien été modifié.";
+                header( "refresh:2;url=index.php?page=member" );
+            }else{
+                $message="Les mots de passe ne correspondent pas.";
+            }
+        }else{
+            $message="L'ancien mot de passe est invalide";
+        }
+
+
+    }else {
+        $message = "Veuillez remplir tous les champs";
+    }
+}
+
+if(isset($_POST['submit'], $_SESSION['user_id'])) {
+    if( !empty($_POST['newvalue'])) {
+        $id = $_SESSION['user_id'];
+        $newvalue = $_POST['newvalue'];
+        if ($value=='user_name'){
+            $set = $bdd->prepare("UPDATE `users` SET `user_name`=? WHERE user_id=?");
+            $set -> execute(array($newvalue, $id));
+            $message="Le nom d'utilisateur a bien été mis à jour.";
+            header( "refresh:2;url=index.php?page=member" );
+        }else if ($value=='user_lastname'){
+            $set = $bdd->prepare("UPDATE `users` SET `user_lastname`=? WHERE user_id=?");
+            $set -> execute(array($newvalue, $id));
+            $message="Le nom de famille a bien été mis à jour.";
+            header( "refresh:2;url=index.php?page=member" );
+        }else if ($value=='user_firstname'){
+            $set = $bdd->prepare("UPDATE `users` SET `user_firstname`=? WHERE user_id=?");
+            $set -> execute(array($newvalue, $id));
+            $message="Le prénom a bien été mis à jour.";
+            header( "refresh:2;url=index.php?page=member" );
+        }else{
+            $set = $bdd->prepare("UPDATE `users` SET `user_email`=? WHERE user_id=?");
+            $set -> execute(array($newvalue, $id));
+            $message="L'adresse E-Mail a bien été mise à jour.";
+            header( "refresh:2;url=index.php?page=member" );
+        }
+    }else {
+        $message = "Veuillez remplir tous les champs";
+    }
+}
+
+if (isset ($message)) {
+    echo $message;
+}
+
+?>
